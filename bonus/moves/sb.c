d7567c32145ffba993ca1c3990e2864033ea4728
yel-alja <yassine122alja@gmail.com>
2025-01-24 08:37:46 +0100

    Update sb.c

--- bonus/moves/sb.c
+++ bonus/moves/sb.c
@@ -22,10 +22,7 @@ void	sb(t_stack **b)
 		return ;
 	first = (*b);
 	second = (*b)->next;
-	if (first->i > second->i)
-	{
-		tmp = first->i;
-		first->i = second->i;
-		second->i = tmp;
-	}
+	tmp = first->i;
+	first->i = second->i;
+	second->i = tmp;
 }
